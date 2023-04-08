8dbcbabb41f83b66a817a194f4a1a52a776bc874
yashar <115088770+yasharzavary@users.noreply.github.com>
2023-04-08 05:53:53 +0430

    mouse move is done, now click

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 # create by: yashar zavary rezaie
 import cv2
 import mediapipe as mp
+import pyautogui
 cam = cv2.VideoCapture(0)
-face_mesh=mp.solutions.face_mesh.FaceMesh()
+face_mesh=mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 while True:
     _, frame = cam.read()
+    frame_y,frame_x,_= frame.shape
     frame=cv2.flip(frame, 1)
     rgb_frame=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output=face_mesh.process(rgb_frame)
     landmark_point=output.multi_face_landmarks
-    print(landmark_point)
+    screenw,screenh=pyautogui.size()
+    if landmark_point:
+        landmarks=landmark_point[0].landmark
+        for lid,landmark in enumerate(landmarks[474:478]):
+            x=int(landmark.x*frame_x)
+            y=int(landmark.y*frame_y)
+            cv2.circle(frame,(x,y),3,(0,255,0))
+            if lid==1:
+                move_x=landmark.x*screenw
+                move_y=landmark.y*screenh
+                pyautogui.moveTo(move_x,move_y)
     cv2.imshow("eye control", frame)
     cv2.waitKey(1)
