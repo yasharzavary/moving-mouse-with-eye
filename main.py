0e09837981bca44403f89caa20afe29cabdb4073
yashar <115088770+yasharzavary@users.noreply.github.com>
2023-04-08 05:32:27 +0430

    landmarks finish...now moving our mouse

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # create by: yashar zavary rezaie
 import cv2
-
+import mediapipe as mp
 cam = cv2.VideoCapture(0)
+face_mesh=mp.solutions.face_mesh.FaceMesh()
 while True:
     _, frame = cam.read()
     frame=cv2.flip(frame, 1)
+    rgb_frame=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output=face_mesh.process(rgb_frame)
+    landmark_point=output.multi_face_landmarks
+    print(landmark_point)
     cv2.imshow("eye control", frame)
     cv2.waitKey(1)
-
-
