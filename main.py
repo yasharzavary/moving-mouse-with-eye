164c8f9fe1e20fb44cb003b85f734457c2492563
yashar <115088770+yasharzavary@users.noreply.github.com>
2023-04-08 05:24:21 +0430

    camera is ok, now reading the face

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # create by: yashar zavary rezaie
+import cv2
+
+cam = cv2.VideoCapture(0)
+while True:
+    _, frame = cam.read()
+    frame=cv2.flip(frame, 1)
+    cv2.imshow("eye control", frame)
+    cv2.waitKey(1)
+
+
